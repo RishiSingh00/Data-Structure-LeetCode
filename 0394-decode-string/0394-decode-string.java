8e10eaddd47980145b744bfdccdaee0970a0b43d
Rishi Singh <singhrishi245021@gmail.com>
2022-10-28 09:49:46 +0530

    Time: 12 ms (5.60%), Space: 43 MB (5.99%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.*;
+class Solution {
+    public String decodeString(String s) {
+        
+        Stack<String> stk = new Stack<>();
+        String ans = "",tempStr="";
+        
+        for(int i=0;i<s.length();i++) {
+            if(stk.empty() && s.charAt(i)>=97 && s.charAt(i)<=122)
+                ans+=s.charAt(i);
+            else if(s.charAt(i)==']') {
+                tempStr = "";
+                
+                while(!stk.peek().equals("[")) {
+                    tempStr=stk.pop()+tempStr;
+                    
+                }
+                stk.pop();   //to pop '['
+                int decimal=0,k=0;
+                while(!stk.empty() && stk.peek().charAt(0)>=48 && stk.peek().charAt(0)<=57) {
+                    int val=Integer.parseInt(String.valueOf(stk.pop()));
+                    if(val!=0) {
+                        k+=Math.pow(10,decimal)*val;
+                    }
+                    decimal++;
+                }
+                
+                tempStr = tempStr.repeat(k);
+                if(stk.empty()) {
+                    ans+=tempStr;
+                }
+                else {
+                    stk.push(tempStr);
+                }
+            }
+            else {
+                
+                stk.push(String.valueOf(s.charAt(i)));
+            }
+            
+        }
+        
+        return ans;
+    }
+}

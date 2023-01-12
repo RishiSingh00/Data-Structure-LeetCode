b752a2ef2b79c2df6d15ed3b11266045d0557da5
Rishi Singh <singhrishi245021@gmail.com>
2023-01-12 20:03:41 +0530

    Time: 6 ms (96.14%), Space: 49.3 MB (63.02%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -3,27 +3,14 @@ class Solution {
         
         int i = 0, j = 0, result = 0;
         int Product = 1;
-        boolean flag = true;
-        while(i <= j && j <=nums.length-1 ) {
-            if(flag)
-                Product *= nums[j];
-            
-            if(Product >= k){
-                Product /= nums[i];
-                if(i == j){
-                    flag = true;
-                    j++;}
-                else
-                    flag = false;
-                i++;
+        while(j <= nums.length-1 ) {
+            Product *= nums[j];
+            while(i <= j && Product >= k){
+                Product /= nums[i++];
                 
-                continue;
             }
-            else    
-                result += j - i + 1;
+            result += j - i + 1;
             j++;
-            System.out.println(i+" "+j+" "+Product);
-            flag = true;
         }
         return result;
     }

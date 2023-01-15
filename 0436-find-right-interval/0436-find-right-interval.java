a112882c3f70d1e3941a327da2b68197412ef1e3
Rishi Singh <singhrishi245021@gmail.com>
2023-01-15 23:34:12 +0530

    Time: 13 ms (97.52%), Space: 46.2 MB (95.19%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,44 +1,36 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        int[] startArr = new int[intervals.length];
-        HashMap<Integer, Integer> startIndexHsh = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int m = intervals.length;
+        int n = intervals[0].length;
+        int[] sp = new int[m];                  //array of starting points
         
-        for( int i = 0 ; i < intervals.length ; i++) {
-            startArr[i] = intervals[i][0];
-            startIndexHsh.put(intervals[i][0],i);
+        for(int i = 0; i < m; i++) {
+            sp[i] = intervals[i][0];            
+            map.put(sp[i], i);                  //(key=start_point, val=index)
         }
-        Arrays.sort(startArr);
         
-        int[] result = new int[intervals.length];
+        Arrays.sort(sp);                        //sort array of starting points
+        int[] result = new int[m];
         
-        for(int i = 0 ; i < intervals.length ; i++) {
-            
-                int res = binarySearch(startArr,intervals[i][1]);
-                result[i] = res == -1 ? -1 : startIndexHsh.get(startArr[res]);    
-            
-            
-        }       
-        
+        for(int i = 0; i < m; i++) {
+            int l = 0, r = m - 1;
+            boolean found = false;              //to see if result was found
+            int min = -1;
+            int ep = intervals[i][n - 1];       //ep = endpoint
+            while(l <= r) {                     //binarySearch on arr of start points
+                int mid = (l + ((r - l) / 2));
+                if(sp[mid] >= ep) {
+                    min = sp[mid];              
+                    found = true;               
+                    r = mid - 1;
+                }
+                else {
+                    l = mid + 1;
+                }
+            }
+            result[i] = found ? map.get(min) : -1;
+        }
         return result;
-        
-    }
-    
-    int binarySearch(int[] arr, int target) {
-        int s = 0, e = arr.length-1;
-        
-        while(s <= e) {
-            int m = s + ( e - s ) / 2; 
-            System.out.println(m+" "+target);
-            if(m == 0 && arr[m] >= target) 
-                return m ;
-            if(arr[m] >= target && arr[m-1] < target)
-                return m ;
-            if(arr[m] > target)
-                e = m - 1;
-            else
-                s = m + 1;
-        }
-        
-        return -1;
     }
 }

0884da4befa70bf3eb08814b746b18bb9ceea837
Rishi Singh <singhrishi245021@gmail.com>
2023-01-28 11:59:16 +0530

    Time: 7 ms (99.02%), Space: 47.6 MB (51.05%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,37 +1,23 @@
-import java.util.*;
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        
-        int idx = 0;
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        ArrayList<Integer> innerArr = new ArrayList<Integer>();
-        innerArr.add(intervals[0][0]);
-        innerArr.add(intervals[0][1]);
-        ans.add(innerArr);
-        
-        for(int i = 1 ; i < intervals.length ; i++) {
-            if(intervals[i][0] <= ans.get(idx).get(1)) {
-                if(intervals[i][1]>ans.get(idx).get(1))
-                    ans.get(idx).add(1,intervals[i][1]);
-            }
-                
-                
-            else {
-                ArrayList<Integer> temp = new ArrayList<>();
-                temp.add(intervals[i][0]);
-                temp.add(intervals[i][1]);
-                ans.add(temp);
-                idx++;
-            }
-        }
-        
-        int[][] result = new int[ans.size()][2];
-        for(int i = 0 ; i < ans.size() ; i++) {
-            result[i][0] = ans.get(i).get(0);
-            result[i][1] = ans.get(i).get(1);
-        }
-        return result;
-        
-    }
+	public int[][] merge(int[][] intervals) {
+		if (intervals.length <= 1)
+			return intervals;
+
+		// Sort by ascending starting point
+		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+		List<int[]> result = new ArrayList<>();
+		int[] newInterval = intervals[0];
+		result.add(newInterval);
+		for (int[] interval : intervals) {
+			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
+			else {                             // Disjoint intervals, add the new interval to the list
+				newInterval = interval;
+				result.add(newInterval);
+			}
+		}
+
+		return result.toArray(new int[result.size()][]);
+	}
 }
